9ddadcb638d949f2d0f98402f71a8213d6a31d81
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-04-28 14:08:22 -0400

    define user commands and default keymaps

--- plugin/glacier.lua
+++ plugin/glacier.lua
@@ -1,1 +1,36 @@
 require("glacier")
+
+-- terminal user commands
+vim.api.nvim_create_user_command("GlacierToggleTerminalTab", function()
+  require("glacier.terminal").toggles.toggle_terminal_tab()
+end, {})
+
+vim.api.nvim_create_user_command("GlacierToggleTerminalCurrentWin", function()
+  require("glacier.terminal").toggles.toggle_terminal_current_win()
+end, {})
+
+vim.api.nvim_create_user_command("GlacierToggleTerminalBottomWin", function()
+  require("glacier.terminal").toggles.toggle_terminal_bottom_win()
+end, {})
+
+-- default keymaps
+vim.keymap.set(
+  "n",
+  "<leader>t<CR>",
+  "<cmd>GlacierToggleTerminalCurrentWin<CR>",
+  { desc = "Glacier: toggle the integrated terminal within the current window" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>t<CR>",
+  "<cmd>GlacierToggleTerminalCurrentWin<CR>",
+  { desc = "Glacier: toggle the integrated terminal within the current window" }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>t<CR>",
+  "<cmd>GlacierToggleTerminalCurrentWin<CR>",
+  { desc = "Glacier: toggle the integrated terminal within the current window" }
+)
